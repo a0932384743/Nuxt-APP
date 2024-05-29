bcdbfeb7ade81ff27f692f10a54ba26d198a82bf
JackyChen <a0932384743@gmail.com>
2024-05-29 13:26:57 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
   server: {
-    port: 3000,
+    port: 80,
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   router: {
