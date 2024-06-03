5ed1dbe78b5009f6c58c197d1ace4829fa19d18a
MSI\user <a0932384743@gmail.com>
2024-06-03 11:47:00 +0800

    增加港口船隻分布

--- plugins/echart/index.ts
+++ plugins/echart/index.ts
@@ -1,15 +1,19 @@
 import Vue from 'vue';
 import ECharts from 'vue-echarts';
+import kaoMap from 'static/geo/高雄市鄉鎮.json';
 import 'echarts';
 import 'echarts/lib/component/geo';
 import 'echarts/lib/component/markLine';
 import 'echarts/lib/component/markPoint';
 import 'echarts/lib/component/markArea';
 import 'static/js/world';
-import { use } from 'echarts/core';
+import { registerMap, use } from 'echarts/core';
 import { CanvasRenderer } from 'echarts/renderers';
 import { LinesChart } from 'echarts/charts';
 import { GeoComponent } from 'echarts/components';
+
+// @ts-ignore
+registerMap('kaoMap', kaoMap);
 
 // Register the required components
 use([CanvasRenderer, LinesChart, GeoComponent]);
